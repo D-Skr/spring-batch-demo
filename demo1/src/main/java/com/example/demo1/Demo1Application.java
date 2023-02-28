489f5b633d58548fae65cbdb2a12c526866427ae
Dmitrii S <altusj@gmail.com>
2023-02-28 15:13:59 -0500

    add demo1 app

--- demo1/src/main/java/com/example/demo1/Demo1Application.java
+++ demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -1,13 +1,55 @@
 package com.example.demo1;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class Demo1Application {
+
+    @Autowired
+    private JobBuilderFactory jobs;
+
+    @Autowired
+    private StepBuilderFactory steps;
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
     }
 
+    @Bean
+    public Step step1(){
+        return steps.get("step1")
+                .tasklet(demoTasklet())
+                .build();
+    }
+
+    private Tasklet demoTasklet() {
+        return (new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("Hello Batch World");
+                return RepeatStatus.FINISHED;
+            }
+        });
+    }
+
+    @Bean
+    public Job demoJob(){
+        return jobs.get("demoJob")
+                .start(step1())
+                .build();
+    }
 }
