917c3fe08e736a46b35ed66f65263a00413c28ed
Dmitrii S <altusj@gmail.com>
2023-03-01 17:43:48 -0500

    add ApplicationProperties

--- patient-batch-loader/src/main/java/com/pluralsight/springbatch/patientbatchloader/config/ApplicationProperties.java
+++ patient-batch-loader/src/main/java/com/pluralsight/springbatch/patientbatchloader/config/ApplicationProperties.java
@@ -1,5 +1,6 @@
 package com.pluralsight.springbatch.patientbatchloader.config;
 
+import org.hibernate.engine.jdbc.batch.spi.Batch;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
@@ -10,5 +11,19 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
 
+    private final Batch batch = new Batch();
 
+    public Batch getBatch() {
+        return batch;
+    }
+
+    public static class Batch {
+        private String inputPath = "c:/Projects/patient-batch-loader/data"
+        private String getInputPath() {
+            return this.inputPath;
+        }
+        public void setInputPath(String inputPath) {
+            this.inputPath = inputPath;
+        }
+    }
 }
