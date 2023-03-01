e4669d04173bdefa5903b90663bba7355f8bd215
Dmitrii S <altusj@gmail.com>
2023-03-01 17:16:49 -0500

    add batch configuration methods

--- patient-batch-loader/src/main/java/com/pluralsight/springbatch/patientbatchloader/config/BatchConfiguration.java
+++ patient-batch-loader/src/main/java/com/pluralsight/springbatch/patientbatchloader/config/BatchConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -46,6 +47,13 @@ public class BatchConfiguration implements BatchConfigurer {
     @Override
     public JobExplorer getJobExplorer() throws Exception {
         return this.jobExplorer;
+    }
+
+    protected JobLauncher createJobLauncher() throws Exception {
+        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+        jobLauncher.setJobRepository(jobRepository);
+        jobLauncher.afterPropertiesSet();
+        return jobLauncher;
     }
 
 
