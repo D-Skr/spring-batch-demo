ebc30218b5c8c8a7214563b7f319a31fc69fe2eb
Dmitrii S <altusj@gmail.com>
2023-03-01 17:13:55 -0500

    add batch configuration

--- patient-batch-loader/src/main/java/com/pluralsight/springbatch/patientbatchloader/config/BatchConfiguration.java
+++ patient-batch-loader/src/main/java/com/pluralsight/springbatch/patientbatchloader/config/BatchConfiguration.java
@@ -2,13 +2,51 @@ package com.pluralsight.springbatch.patientbatchloader.config;
 
 import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Component
 @EnableBatchProcessing
 public class BatchConfiguration implements BatchConfigurer {
 
-    private JobRepositoryjobRepository;
-    private JobExplorerjobExplorer;
-    private JobLauncherjobLauncher;
+    private JobRepository jobRepository;
+    private JobExplorer jobExplorer;
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    @Qualifier(value = "batchTransactionManager")
+    private PlatformTransactionManager batchTransactionManager;
+
+    @Autowired
+    @Qualifier(value = "batchDataSource")
+    private DataSource batchDataSource;
+
+    @Override
+    public JobRepository getJobRepository() throws Exception {
+        return this.jobRepository;
+    }
+
+    @Override
+    public PlatformTransactionManager getTransactionManager() throws Exception {
+        return this.batchTransactionManager;
+    }
+
+    @Override
+    public JobLauncher getJobLauncher() throws Exception {
+        return this.jobLauncher;
+    }
+
+    @Override
+    public JobExplorer getJobExplorer() throws Exception {
+        return this.jobExplorer;
+    }
+
+
 }
