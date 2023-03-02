34b34362a703c78447e5c3af3db8a8e75b7d72fc
Dmitrii S <altusj@gmail.com>
2023-03-02 10:56:38 -0500

    add constants and validator

--- patient-batch-loader/src/main/java/com/pluralsight/springbatch/patientbatchloader/config/BatchJobConfiguration.java
+++ patient-batch-loader/src/main/java/com/pluralsight/springbatch/patientbatchloader/config/BatchJobConfiguration.java
@@ -30,4 +30,29 @@ public class BatchJobConfiguration {
             .start(step)
             .build();
     }
+    @Bean
+    public JobParametersValidator validator() {
+        return new JobParametersValidator() {
+            @Override
+            public void validate(JobParameters parameters) throws JobParametersInvalidException {
+                String fileName = parameters.getString(Constants.JOB_PARAM_FILE_NAME);
+                if (StringUtils.isBlank(fileName)) {
+                    throw new JobParametersInvalidException(
+                        "The patient-batch-loader.fileName parameter is required.");
+                }
+                try {
+                    Path file = Paths.get(applicationProperties.getBatch().getInputPath() +
+                        File.separator + fileName);
+                    if (Files.notExists(file) || !Files.isReadable(file)) {
+                        throw new Exception("File did not exist or was not readable");
+                    }
+                } catch (Exception e) {
+                    throw new JobParametersInvalidException(
+                        "The input path + patient-batch-loader.fileName parameter needs to " +
+                            "be a valid file location.");
+                }
+            }
+        };
+    }
+
 }
