4427d44ee96d30bbf5bda3a6445eddeedabed3f3
Dmitrii S <altusj@gmail.com>
2023-03-02 13:10:09 -0500

    add constant and LineMapper

--- patient-batch-loader/src/main/java/com/pluralsight/springbatch/patientbatchloader/config/BatchJobConfiguration.java
+++ patient-batch-loader/src/main/java/com/pluralsight/springbatch/patientbatchloader/config/BatchJobConfiguration.java
@@ -7,14 +7,22 @@ import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.PathResource;
 
 import java.io.File;
 import java.nio.file.Files;
@@ -94,5 +102,36 @@ public class BatchJobConfiguration {
             .writer(writer())
             .build();
     }
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<PatientRecord> reader(
+        @Value("#{jobParameters['" + Constants.JOB_PARAM_FILE_NAME + "']}")String fileName) {
+        return new FlatFileItemReaderBuilder<PatientRecord>()
+            .name(Constants.ITEM_READER_NAME)
+            .resource(
+                new PathResource(
+                    Paths.get(applicationProperties.getBatch().getInputPath() +
+                        File.separator + fileName)))
+            .linesToSkip(1)
+            .lineMapper(lineMapper())
+            .build();
+    }
+
+    @Bean
+    public LineMapper<PatientRecord> lineMapper() {
+        DefaultLineMapper<PatientRecord> mapper = new DefaultLineMapper<>();
+        mapper.setFieldSetMapper((fieldSet) -> new PatientRecord(
+            fieldSet.readString(0), fieldSet.readString(1),
+            fieldSet.readString(2), fieldSet.readString(3),
+            fieldSet.readString(4), fieldSet.readString(5),
+            fieldSet.readString(6), fieldSet.readString(7),
+            fieldSet.readString(8), fieldSet.readString(9),
+            fieldSet.readString(10), fieldSet.readString(11),
+            fieldSet.readString(12)));
+        mapper.setLineTokenizer(new DelimitedLineTokenizer());
+        return mapper;
+    }
+
 
 }
