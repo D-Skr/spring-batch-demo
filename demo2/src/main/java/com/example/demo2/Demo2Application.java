27f9b66e033ddd3e596bedd15d19c4bfe8780d46
Dmitrii S <altusj@gmail.com>
2023-03-06 18:20:39 -0500

    add 3 packages

--- demo2/src/main/java/com/example/demo2/Demo2Application.java
+++ demo2/src/main/java/com/example/demo2/Demo2Application.java
@@ -7,7 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableBatchProcessing
-@ComponentScan({"com.example.config","com.example.service","com.example.listener"})
+@ComponentScan({"com.example.config","com.example.service","com.example.listener",
+        "com.example.reader","com.example.writer","com.example.processor"})
 public class Demo2Application {
 
     public static void main(String[] args) {
