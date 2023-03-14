c5c01950507373464eefd12c25b54239d04facb7
Dmitrii S <altusj@gmail.com>
2023-03-13 23:43:03 -0400

    add first item writer method

--- demo2/src/main/java/com/example/writer/FirstItemWriter.java
+++ demo2/src/main/java/com/example/writer/FirstItemWriter.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class FirstItemWriter implements ItemWriter<Long> {
     @Override
     public void write(List<? extends Long> items) throws Exception {
-
+        System.out.println("Inside Item Writer");
+        items.stream().forEach(System.out::println);
     }
 }
