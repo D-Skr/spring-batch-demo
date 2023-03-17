1744deb7f4ff8c78735945b18c26b5ac4bb5541e
Dmitrii S <altusj@gmail.com>
2023-03-17 17:20:47 -0400

    add job stop

--- demo2/src/main/java/com/example/controller/JobController.java
+++ demo2/src/main/java/com/example/controller/JobController.java
@@ -2,21 +2,14 @@ package com.example.controller;
 
 import com.example.request.JobParamsRequest;
 import com.example.service.JobService;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameter;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobOperator;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/job")
@@ -25,6 +18,9 @@ public class JobController {
     @Autowired
     JobService jobService;
 
+    @Autowired
+    JobOperator jobOperator;
+
 
     @GetMapping("/start/{jobName}")
     public String startJob(@PathVariable String jobName,
@@ -32,5 +28,15 @@ public class JobController {
 
         jobService.startJob(jobName, jobParamsRequestList);
         return "Job started...";
+    }
+
+    @GetMapping("/stop/{jobExecutionId}")
+    public String stopJob(@PathVariable long jobExecutionId) {
+        try {
+            jobOperator.stop(jobExecutionId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Job stopped!";
     }
 }
