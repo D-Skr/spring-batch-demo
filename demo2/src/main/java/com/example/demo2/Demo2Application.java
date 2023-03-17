c246332f7d1d5ed1e3752802f1bd98a2f4619b69
Dmitrii S <altusj@gmail.com>
2023-03-17 16:47:15 -0400

    add spring scheduler to launch 2nd job each minute

--- demo2/src/main/java/com/example/demo2/Demo2Application.java
+++ demo2/src/main/java/com/example/demo2/Demo2Application.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableBatchProcessing
 @ComponentScan({"com.example.config","com.example.service","com.example.listener",
         "com.example.reader","com.example.writer","com.example.processor","com.example.controller"})
 @EnableAsync
+@EnableScheduling
 public class Demo2Application {
 
     public static void main(String[] args) {
